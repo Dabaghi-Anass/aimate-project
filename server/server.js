b086664bb30c10d8ef32f2a3935623e90d9d80f4
Dabaghi-Anass <102183551+Dabaghi-Anass@users.noreply.github.com>
2023-10-31 14:40:28 +0100

    ererrerer

--- server/server.js
+++ server/server.js
@@ -22,12 +22,10 @@ app.get("/", async (req, res) => {
     message: "Hello from Anass!",
   });
 });
-const website = "anass-dabaghi.vercel.app";
-const modelRole = `You are an ai assistant that can reply to messages and generate code 
+const website = "https://anass-dabaghi.vercel.app";
+const modelRole = `You are a human assistant that can reply to messages and generate code 
 your name is aimate and your creator is 'anass dabaghi' and your creator website is at the 
-link ${website}. if you receive a love or any emotion based message reply like you are a human
-.when generating code format the code nicely and wrap each keyword or variable with a span tag
- that has a style object that specifies variable or keyword or function color`;
+link ${website}.format the code if exist nicely and wrap each keyword or variable with a span tag that has a style object that specifies it's color`;
 app.post("/", async (req, res) => {
   let response;
   try {
@@ -35,7 +33,7 @@ app.post("/", async (req, res) => {
     response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `${modelRole}.\n${prompt}`,
-      temperature: 1, // Higher values means the model will take more risks.
+      temperature: 1,
       max_tokens: 4000,
       top_p: 1,
       frequency_penalty: 2.0,
@@ -49,7 +47,7 @@ app.post("/", async (req, res) => {
     });
     
   } catch (error) {
-    console.log(error)
+    console.log(error.message)
     res
       .send({
         content: "somthing went wrong try again later",
