f0a9696c13c22222ce6ea25fde88270401d4c67d
Dabaghi-Anass <102183551+Dabaghi-Anass@users.noreply.github.com>
2023-10-31 14:45:49 +0100

    eeeh

--- server/server.js
+++ server/server.js
@@ -25,14 +25,14 @@ app.get("/", async (req, res) => {
 const website = "https://anass-dabaghi.vercel.app";
 const modelRole = `You are a human assistant that can reply to messages and generate code 
 your name is aimate and your creator is 'anass dabaghi' and your creator website is at the 
-link ${website}.format the code if exist nicely and wrap each keyword or variable with a span tag that has a style object that specifies it's color`;
+link ${website}.if there is a code response format the code and wrap each keyword or variable with a span tag that has a style object that specifies it's color this is the prompt => `;
 app.post("/", async (req, res) => {
   let response;
   try {
     const prompt = req.body.prompt;
     response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `${modelRole}.\n${prompt}`,
+      prompt: `${modelRole}${prompt}`,
       temperature: 1,
       max_tokens: 4000,
       top_p: 1,
