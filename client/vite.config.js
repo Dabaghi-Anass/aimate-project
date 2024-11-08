785d03426a73f18a6f21d9f1948fe3d7b32ff1e2
Dabaghi-Anass <anass.debbaghi123@gmail.com>
2024-11-08 20:32:50 +0100

    adjusted paths of vfile

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,27 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import path from "path";
+import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "./runtimeConfig": "./runtimeConfig.browser",
-    },
-  },
-  define: {
-    "process.env": {},
-  },
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"#minpath": path.resolve(
+				__dirname,
+				"node_modules/vfile/lib/minpath.browser"
+			),
+			"#minproc": path.resolve(
+				__dirname,
+				"node_modules/vfile/lib/minproc.browser"
+			),
+			"#minurl": path.resolve(
+				__dirname,
+				"node_modules/vfile/lib/minurl.browser"
+			),
+			"./runtimeConfig": "./runtimeConfig.browser",
+		},
+	},
+	define: {
+		"process.env": {},
+	},
 });
